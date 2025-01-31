484db9e28def1f01f81b044d6c8a5c23e189c0cf
7SepRash <jasleen_k@ee.iitr.ac.in>
2025-01-31 19:42:09 +0530

    basic-ui-loans-borrows-done

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -20,12 +20,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Theme appearance="dark">
-          <div style={{zIndex:10,position:"relative"}} className="w-full flex justify-end px-5 py-5">
-             <Button color="orange" variant="soft" style={{borderRadius:"15px"}}>
-              Get Started
-             </Button>
-          </div>
-          <Container className="h-screen w-screen flex flex-col items-center justify-center">
+          <Container >
             {children}
           </Container>
         </Theme>
